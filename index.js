2488713d187336b554ee40b1e06be4909c8c0118
naruebes-dearw <naruebes.phudeth@gmail.com>
2021-11-13 18:31:52 +0700

    fixed wrong client path

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ mongoose.connect(keys.MONGODB_URI, () => {
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }
 
