c62620a88faabb3cd1c1cbdf10a96d59c642fd1a
naruebes-dearw <naruebes.phudeth@gmail.com>
2021-11-15 12:06:17 +0700

    login with facebook success && delete debug code lines

--- config/passport-strategy/passport-facebook.js
+++ config/passport-strategy/passport-facebook.js
@@ -13,15 +13,11 @@ const FACEBOOK_APP_SECRET = "f38cd7199c02b51f9d0413a7663e32f3";
 passport.use(new FacebookStrategy({
   clientID: FACEBOOK_APP_ID,
   clientSecret: FACEBOOK_APP_SECRET,
-  // callbackURL: "/auth/posts",
-  // callbackURL: "/auth/login/facebook/callback",
   callbackURL: "https://facebookcloneproject.herokuapp.com/auth/login/facebook/callback",
   profileFields: ['id', 'displayName', 'picture.type(large)', 'first_name', 'last_name', 'middle_name']
 },
   async (accessToken, refreshToken, profile, done) => {
     await User.findOne({ facebookId: profile.id }, async (err, user) => {
-      // console.log("=========== passport facebook oauth : profile ============")
-      // console.log(profile);
       if (!user) {
         const newUser = await new User({
           facebookId: profile.id,
